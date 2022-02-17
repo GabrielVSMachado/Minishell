8496663c7c00c7e08ecf3f0dc9a83794e32db2b2
gvitor-s <gvitor-s@student.42sp.org.com>
2022-02-17 00:47:54 -0300

    feat: add test to check tokenizer with RINPUT

--- Tests/tests_tokenizer.c
+++ Tests/tests_tokenizer.c
@@ -22,3 +22,48 @@ Test(tokenizer, expected_llist_with_two_words) {
 	clear_tokens(&head);
 	free((void *)line);
 }
+
+Test(tokenizer, expected_llist_with_fours_words_and_one_rinput) {
+	char const	line[] = "tr '\n' ' ' < command.txt";
+	struct s_tokens	*head = NULL;
+
+	head = tokenizer(line);
+	cr_assert_str_eq(head->value, "tr",
+			"head->value on second test: %s\n", head->value);
+	cr_assert(head->token == T_WORD,
+			"head->token on second test: %d\n", head->token);
+
+
+	cr_assert_str_eq(head->next->value, "'\n'",
+			"head->next->value on second test: %s\n", head->next->value);
+	cr_assert(head->next->token == T_WORD,
+			"head->next->token on second test: %d\n", head->next->token);
+
+
+	cr_assert_str_eq(head->next->next->value, "' '",
+		"head->next->next->value on second test: %s\n", head->next->next->value);
+	cr_assert(head->next->next->token == T_WORD,
+		"head->next->next->token on second test: %d\n", head->next->next->token);
+
+
+	cr_assert_null(head->next->next->next->value,
+		"head->next->next->next->next->value is not null on second test");
+	cr_assert(
+		head->next->next->next->token == T_RINPUT,
+		"head->next->next->next->next->token on second test: %d\n",
+		head->next->next->next->token
+		);
+
+
+	cr_assert_str_eq(
+			head->next->next->next->next->value,
+			"command.txt",
+		"head->next->next->next->next->next->next->next->next->value on second test: %s\n",
+		head->next->next->next->next->value);
+	cr_assert(
+		head->next->next->next->next->token == T_WORD,
+		"head->next->next->next->next->next->next->next->next->token on second test: %d\n",
+		head->next->next->next->next->token);
+
+	clear_tokens(&head);
+}
