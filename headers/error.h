dfa506acd9d53f8526e8647df1fc86477ed5552e
gvitor-s <gvitor-s@student.42sp.org.com>
2022-03-09 19:40:17 -0300

    feat: add fuction to handle error on init_envs

--- headers/error.h
+++ headers/error.h
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 19:43:08 by gvitor-s          #+#    #+#             */
-/*   Updated: 2022/02/14 19:43:08 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2022/03/09 14:51:45 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,5 @@
 # include "tokenizer.h"
 
 int	raise_tokenizer_err(char const *msg, struct s_tokens **info);
+int	raise_env_error(void);
 #endif
--- src/errors/err.c
+++ src/errors/err.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error.c                                            :+:      :+:    :+:   */
+/*   err.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 19:02:29 by gvitor-s          #+#    #+#             */
-/*   Updated: 2022/02/14 19:02:29 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2022/03/09 14:57:40 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,20 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "hashtable.h"
 
 int	raise_tokenizer_err(char const *msg, struct s_tokens **info)
 {
 	if (info != NULL && *info != NULL)
 		clear_tokens(info);
 	ft_putendl_fd((char *)msg, STDERR_FILENO);
+	return (-1);
+}
+
+int	raise_env_error(void)
+{
+	destroy_hashtbl();
+	ft_putendl_fd("Wrong allocation on environments variables\n",
+		STDERR_FILENO);
 	return (-1);
 }
