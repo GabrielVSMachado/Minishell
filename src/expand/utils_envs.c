4b71c03b7544bf1fe36918f528ab7e2d24dcf661
gvitor-s <gvitor-s@student.42sp.org.com>
2022-03-11 00:33:10 -0300

    change name of init_envs to utils_envs

--- src/expand/utils_envs.c
+++ src/expand/utils_envs.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init_envs.c                                        :+:      :+:    :+:   */
+/*   utils_envs.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gvitor-s <gvitor-s>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 14:23:03 by gvitor-s          #+#    #+#             */
-/*   Updated: 2022/03/09 19:23:25 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2022/03/10 22:50:29 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,4 +37,35 @@ int	init_envs(void)
 		tmp += 1;
 	}
 	return (0);
+}
+
+unsigned int	len_env_name(char *env_name)
+{
+	char	*tmp;
+
+	tmp = env_name;
+	tmp += 1;
+	if (ft_isdigit(*tmp) || *tmp == '?')
+		return (1);
+	while (*tmp && (ft_isalnum(*tmp) || *tmp == '_'))
+		tmp += 1;
+	return (tmp - (env_name + 1));
+}
+
+char	*treat_envs(char *env_name)
+{
+	unsigned int	len_name;
+	struct s_keyval	*p_env;
+	char			*tmp;
+
+	tmp = env_name;
+	len_name = len_env_name(env_name);
+	tmp += 1;
+	tmp = ft_memcpy(malloc(len_name + 1), tmp, len_name);
+	tmp[len_name] = 0;
+	p_env = hashtbl_lookup(tmp);
+	free(tmp);
+	if (NOT p_env)
+		return (ft_strdup(""));
+	return (ft_strdup(p_env->val));
 }
