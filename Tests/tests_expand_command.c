5c40882acfa13c395fd08cefc44632c1eb284a65
gvitor-s <gvitor-s@student.42sp.org.com>
2022-03-22 20:36:50 -0300

    Fix: change parameter of function expand_command

--- Tests/tests_expand_command.c
+++ Tests/tests_expand_command.c
@@ -11,23 +11,7 @@ Test(expand_command, expected_result_eq_expected_value_variable) {
 	init_hashtbl();
 	result = tokenizer("echo $USER");
 	insert_hashtbl(strdup("USER"), strdup("42sp"));
-	expand_command(result);
-	int i = 0;
-	for (struct s_tokens *tmp = result; tmp; tmp = tmp->next) {
-		cr_assert_str_eq(tmp->value, expected_value[i++]);
-	}
-	destroy_hashtbl();
-	clear_tokens(&result);
-}
-
-Test(expand_command, expected_result_eq_expected_value_variable_1) {
-	const char *expected_value[] = {"echo", "42sp"};
-	struct s_tokens	*result = NULL;
-
-	init_hashtbl();
-	result = tokenizer("echo $USER");
-	insert_hashtbl(strdup("USER"), strdup("42sp"));
-	expand_command(result);
+	expand_command(&result->next->value);
 	int i = 0;
 	for (struct s_tokens *tmp = result; tmp; tmp = tmp->next) {
 		cr_assert_str_eq(tmp->value, expected_value[i++]);
