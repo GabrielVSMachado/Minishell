737939d5218efe4e0bab973c74fa1d4206a52fa8
gvitor-s <gvitor-s@student.42sp.org.com>
2022-03-25 14:42:04 -0300

    Close #19: Add function to msg_of_eof and expand quotes for heredoc delimiter

--- src/executor/heredoc.c
+++ src/executor/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 13:19:30 by gvitor-s          #+#    #+#             */
-/*   Updated: 2022/03/24 22:07:23 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2022/03/25 14:35:32 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,16 @@
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include "expand_str.h"
+
+static void	heredoc_eof_abort_msg(char *delimiter)
+{
+	ft_putstr_fd(
+		"minishell: warning: here-document delimited by end-of-file (wanted ",
+		STDERR_FILENO);
+	ft_putstr_fd(delimiter, STDERR_FILENO);
+	ft_putendl_fd(")", STDERR_FILENO);
+}
 
 int	heredoc(struct s_io *infile)
 {
@@ -26,6 +36,7 @@ int	heredoc(struct s_io *infile)
 	char	*line;
 	int		save;
 
+	expand_quotes(&infile->file);
 	save = errno;
 	(void)pipe(_pipe);
 	line = readline(">");
@@ -35,6 +46,8 @@ int	heredoc(struct s_io *infile)
 		free(line);
 		line = readline(">");
 	}
+	if (line == NULL)
+		heredoc_eof_abort_msg(infile->file);
 	free(line);
 	line = NULL;
 	(void)close(_pipe[1]);
