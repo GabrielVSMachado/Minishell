396be82923d414526dadd04be55155273b09f96f
gvitor-s <gvitor-s@student.42sp.org.com>
2022-03-27 13:39:02 -0300

    Refactory: Main use new logic of exec

--- src/main.c
+++ src/main.c
@@ -6,13 +6,14 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 20:41:00 by gvitor-s          #+#    #+#             */
-/*   Updated: 2022/03/25 15:30:36 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2022/03/27 13:22:44 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <stdio.h>
 #include "executor.h"
+#include "executor/utils_exec.h"
 #include "ft_string.h"
 #include "hashtable.h"
 #include "libft.h"
@@ -22,6 +23,7 @@
 #include "expand/envs.h"
 #include <readline/history.h>
 #include <readline/readline.h>
+#include <stdlib.h>
 
 int	main(void)
 {
@@ -33,17 +35,18 @@ int	main(void)
 	init_envs();
 	while (TRUE)
 	{
-		setup_signals(SIG_IGN, handler_parent);
+		setup_signal(SIGQUIT, SIG_IGN);
+		setup_signal(SIGINT, handler_parent);
 		line = readline("gvitor-s/f-tadeu@42sp[ minishell ]$ ");
 		if (NOT line)
 			return (0);
 		tokens = tokenizer(line);
+		free(line);
 		programs = parsing(tokens);
 		clear_tokens(&tokens);
-		if (programs)
+		if (exec_heredocs(programs) != -1)
 			executor(programs);
 		destroy_programs(&programs);
-		free(line);
 	}
 	return (0);
 }
